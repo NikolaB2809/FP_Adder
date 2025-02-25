1fb4463a156ed90a831f5de12960f7fb0ab4f989
Nikola Babic <nikolab4@gmail.com>
2025-02-25 22:20:56 +0100

    Updated Shift_reg RTL code and testbench

--- verilog/rtl/Shift_reg.v
+++ verilog/rtl/Shift_reg.v
@@ -5,35 +5,45 @@ module Shift_reg(
     input en_in,
     input wr_in,
     output reg input_rdy,
-    output reg [31:0] parallel_out
+    output reg [15:0] parallel_out
 );
 
-reg [5:0] count;
-reg [31:0] value;
+localparam WIDTH = 16;
+
+reg [3:0] count;
+reg [WIDTH-1:0] value;
 integer i;
 
 always @(posedge(clk_in) or posedge(rst_in)) begin
     if (rst_in == 1'b1) begin
         parallel_out <= 32'b0;
         input_rdy <= 1'b1;
-        count <= 5'b0;
-        value <= 32'h00000000;
+        count <= 4'b000;
+        value <= 16'h0000;
     end else begin
-        if (count > 31) begin
+        if (count == 4'b1111) begin
+            count <= 4'b0000;
             input_rdy <= 1'b1;
-            count <= 5'b0;
         end else if (wr_in == 1'b1) begin
-            input_rdy <= 1'b0;
-            for (i = 31; i > 0; i = i - 1) begin
+            for (i = WIDTH - 1; i > 0; i = i - 1) begin
                 value[i - 1] <= value[i];
             end
-            value[31] <= serial_in;
-            count <= count + 1;
+            value[WIDTH-1] <= serial_in;
+            if (count == 4'b1111) begin
+                count <= 4'b0000;
+                input_rdy <= 1'b1;
+            end else begin
+                if (input_rdy == 1'b1) begin
+                    input_rdy <= 1'b0;
+                end else begin
+                    count <= count + 1;
+                end
+            end
         end
         if (en_in == 1'b1) begin
             parallel_out <= value;
         end else begin
-            parallel_out <= 32'h00000000;
+            parallel_out <= 16'h0000;
         end
     end
 end
--- verilog/rtl/Shift_reg_tb.v
+++ verilog/rtl/Shift_reg_tb.v
@@ -1,11 +1,14 @@
 module Shift_reg_tb;
 
+localparam WIDTH = 16;
+
 reg serial_in;
 reg clk_in;
 reg rst_in;
+reg en_in;
 reg wr_in;
 wire input_rdy;
-wire [31:0] parallel_out;
+wire [WIDTH-1:0] parallel_out;
 
 integer i;
 
@@ -13,6 +16,7 @@ Shift_reg dut(
     .serial_in(serial_in),
     .clk_in(clk_in),
     .rst_in(rst_in),
+    .en_in(en_in),
     .wr_in(wr_in),
     .input_rdy(input_rdy),
     .parallel_out(parallel_out)
@@ -23,11 +27,12 @@ always begin
 end
 
 task check;
-        input [31:0] in;  // Inputs to apply
-        input [31:0] expected;  // Expected result
+        input [WIDTH-1:0] in;  // Inputs to apply
+        input [WIDTH-1:0] expected;  // Expected result
         begin
             wr_in <= 1'b1;
-            for(i = 0; i < 32; i = i + 1) begin
+            en_in <= 1'b0;
+            for(i = 0; i < WIDTH; i = i + 1) begin
                 serial_in = in[i];
                 if (input_rdy == 1'b1 && i > 0) begin
                     $display("Test failed for in = 0x%0h: input_rdy = %d; expected: 0; iteration: %d", in, input_rdy, i);
@@ -36,7 +41,12 @@ task check;
             end
             wr_in <= 1'b0;
             #10;
-            if (parallel_out !== expected) begin
+            if (parallel_out != 16'h0000) begin
+                $display("Test failed for in = 0x%0h: expected: 0x0000, got: 0x%0h", in, parallel_out);
+            end
+            en_in <= 1'b1;
+            #10;
+            if (parallel_out != expected) begin
                 $display("Test failed for in = 0x%0h: expected: 0x%0h, got: 0x%0h", in, expected, parallel_out);
             end else begin
                 $display("Test passed for in = 0x%0h; Result: 0x%0h", in, parallel_out);
@@ -48,17 +58,20 @@ task check;
     endtask
 
 initial begin
-    serial_in = 1'b0;
-    clk_in = 1'b0;
-    rst_in = 1'b1;
-    wr_in = 1'b0;
+    $dumpfile("./build/Shift_reg_tb.vcd");
+    $dumpvars(0, Shift_reg_tb);
+    serial_in <= 1'b0;
+    clk_in <= 1'b0;
+    rst_in <= 1'b1;
+    wr_in <= 1'b0;
+    en_in <= 1'b0;
     #10;
-    rst_in = 0;
+    rst_in <= 0;
     #10;
-    check(32'h00000000, 32'h00000000);
-    check(32'haaaaaaaa, 32'haaaaaaaa);
-    check(32'habcd1110, 32'habcd1110);
-    check(32'hf89123de, 32'hf89123de);
+    check(16'h0000, 16'h0000);
+    check(16'hffff, 16'hffff);
+    check(16'habcd, 16'habcd);
+    check(16'h6789, 16'h6789);
     $finish;
 end
 
